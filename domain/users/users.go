94e93e8c33f3b549f60175c58dc1e86acd558847
keumda <dayeyae@gmail.com>
2021-01-27 11:34:55 +0900

    fix :  (blank space) correct minor typos in code (#1)
    details
    
    #in-progress SWIT-1-BUG
    
    Co-authored-by: sophie <keumda@github.com>

--- domain/users/users.go
+++ domain/users/users.go
@@ -16,6 +16,7 @@ type User struct {
 func (user *User) Validate() *errors.RestErr {
 	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
 	if user.Email == "" {
+
 		return errors.NewBadRequestError("invalid email address")
 	}
 	return nil
