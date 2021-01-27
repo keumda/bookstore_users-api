4948ab5b61b620901ffacb3d481e403035598e69
sophie <keumda@github.com>
2021-01-27 12:47:07 +0900

    fix : (blank space) correct minor typos in code
    details
    
    SWIT-2 #close #comment Fixed this today

--- domain/users/users.go
+++ domain/users/users.go
@@ -9,7 +9,7 @@ type User struct {
 	Id          int64  `json:"id"`
 	FirstName   string `json:"first_name"`
 	LastName    string `json:"last_name"`
-	Email       string `json:"email"`
+	Email       string `json:"e_mail"`
 	DateCreated string `json:"date_created"`
 }
 
